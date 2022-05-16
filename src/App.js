e26637ec80f95c185ffcf2183d712f803e8b2639
JUNYA-727 <k.junya0727@icloud.com>
2022-05-17 01:55:12 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import {BrowserRouter as Router,Route} from "react-router-dom";
-import Home from "./Home";
-import About from './About';
 function App() {
   return (
     <Router>

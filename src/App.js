972bce7ec8d24aae5578600e027f00909e1ba92a
JUNYA-727 <k.junya0727@icloud.com>
2022-05-17 02:30:34 +0900

    main

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <Router>
       <Route exact path={"/"} component={Home}></Route>
+      <Route exact path={"/about"} component={About}></Route>
     </Router>
   );
 }
